b6dd669f9a9d5abbca80173860b34a2db3dbf785
push42 <thiesmk2@gmail.com>
2023-07-31 19:44:08 +0200

    modified:   index.php 	deleted:    js/soundbutton.js

--- index.php
+++ index.php
@@ -2169,6 +2169,20 @@ function createTaskItem(taskId, taskContent) {
 }
 
 document.addEventListener('DOMContentLoaded', loadTasks);
+
+// Sound Button
+// Get a reference to the button and the audio element
+var button = document.getElementById('header-info-right-login');
+var audio = document.getElementById('mySound2');
+//
+var button = document.getElementById('joinchat-button');
+var audio = document.getElementById('mySound');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+// Play the audio
+audio.play();
+});
   </script>
   <script type="text/javascript" src="js/serverrestart.js"></script>
 </body>
